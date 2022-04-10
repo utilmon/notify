28e163164f4d948d562235508b87d944afffa0a6
EC2 Default User <ec2-user@ip-172-31-46-132.us-east-2.compute.internal>
2022-04-10 20:24:07 +0000

    initial

---
+++ trigger/uscis/alert.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime, timezone
+import yaml
+import os
+import sys
+
+sys.path.append(f"{os.path.dirname(__file__)}/../../api")
+import gmail
+import checkUSCIS as uscis
+import json
+import copy
+
+receipt_numbers = ["MSC2290390290", "MSC2290390292", "MSC2290390291"]
+
+
+def record_status(new_status: dict):
+
+    new_status["date"] = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+
+    path = f"{os.path.dirname(__file__)}/history.yaml"
+    history = yaml.safe_load(open(path, "r"))
+    history.insert(0, new_status)
+    yaml.dump(history, open(path, "w"))
+
+
+def check_msg(input:dict, case_num:str):
+
+    output = input
+
+    status = uscis.requestStatus(case_num)
+
+    output[case_num] = status
+    
+    return output
+
+def update_status(input:dict, case_numbers: list):
+
+    output = copy.deepcopy(input)
+
+    for case_num in case_numbers:
+        output = check_msg(output, case_num)
+
+    return output
+
+def read_msg():
+    history = yaml.safe_load(open(os.path.dirname(__file__) + "/history.yaml", "r"))
+    return history[0]
+
+
+if __name__ == "__main__":
+
+    old_status = read_msg()
+    new_status = update_status(old_status, receipt_numbers)
+
+    if new_status != old_status:
+        msg = json.dumps(new_status, indent=4)
+        gmail.send_strmsg(title="Python: USCIS update", msg=msg)
+
+        record_status(new_status)
+    
---
+++ trigger/uscis/uscis_trigger.sh
@@ -0,0 +1,2 @@
+source ~/installs/mambaforge/bin/activate notify
+python3 ~/git/notify/trigger/uscis/alert.py
